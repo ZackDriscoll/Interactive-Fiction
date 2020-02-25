ef48288af9636dbed48fee4d9e2e8f58438a2489
Zack Driscoll <zacdrisc@uat.edu>
2020-02-25 14:21:01 -0700

    Created prototype sum function.

--- Interactive Fiction/Source.cpp
+++ Interactive Fiction/Source.cpp
@@ -6,10 +6,33 @@
 
 using namespace std;
 
+//prototype functions
+int AddTwoNumbers(int firstNumber, int secondNumber);
+
 int main()
 {
+	cout << "\n\n\t\t *** Welcome to my calculator. ***\n\n";
+
+	//We need a variable to catch the sum which is the return value
+	int catchTheSum;
 	
+	//Calling the function
+	catchTheSum = AddTwoNumbers(4, 10);
 
-
+	//Display the answer to the screen
+	cout << "\nMy calculations say the sum is " << catchTheSum << endl;
+	
 	return 0;
+}
+
+int AddTwoNumbers(int firstNumber, int secondNumber)
+{
+	//Print what the function does
+	cout << "\nI am calculating two numbers...\n";
+	
+	//Create variable to hold the sum and add the two numbers together
+	int sum = firstNumber + secondNumber;
+
+	//Return the sum fo the numbers from the function
+	return sum;
 }
